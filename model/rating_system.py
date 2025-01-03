e0fa267bb411ed0ead9b38d0deb44ccaf1a4962c
ansonkwokth <87415442+ansonkwokth@users.noreply.github.com>
2025-01-03 19:20:56 +0100

    added comments; add protections; rename model;

--- model/rating_system.py
+++ model/rating_system.py
@@ -9,7 +9,7 @@ class ProbCalculator:
     def _add_sigma(sigma1, sigma2):
         return np.sqrt(sigma1**2 + sigma2**2)
 
-        
+
 
     @staticmethod
     def predict_set_config_from_p(p):
@@ -28,6 +28,9 @@ class ProbCalculator:
 
     @staticmethod
     def predict_game_config_from_p(p, n_win_sets=3):
+        # Ensure n_win_sets is odd
+        if n_win_sets % 2 == 0:
+            raise ValueError("'n_win_sets' must be an odd number.")
         q = 1 - p
         dt = {}
         # Calculate probabilities for winning n_win_sets, n_win_sets+1, ...
@@ -40,7 +43,8 @@ class ProbCalculator:
 
 
 
-class RatingSystem(ProbCalculator):
+
+class EloLike(ProbCalculator):
     def __init__(self, learning_rate=0.01, base_param=(0, 1), update_sigma=True, verbose=False):
         """
         Initialize the rating system.
@@ -48,6 +52,27 @@ class RatingSystem(ProbCalculator):
         :param params: Parameters for the players.
         :param learning_rate: Learning rate.
         """
+        if not isinstance(learning_rate, (float, int)):
+            raise TypeError(f"Expected 'learning_rate' to be an float/int, but got {type(learning_rate).__name__}")
+
+        if learning_rate <= 0:
+            raise ValueError(f"Expected 'learning_rate' to be positive, but got {learning_rate}")
+
+        if not isinstance(base_param, tuple):
+            raise TypeError(f"Expected 'base_param' to be a tuple, but got {type(base_param).__name__}")
+        if len(base_param) != 2:
+            raise ValueError(f"Expected 'value' to be a 2-entry tuple, but got {len(base_param)} entries")
+        if not all(isinstance(entry, (int, float)) for entry in base_param):
+            raise TypeError("Both entries in 'base_param' must be float/int")
+        if base_param[1] <= 0:
+            raise ValueError(f"The second entry in 'base_param' must be greater than 0, but got {base_param[1]}")
+        
+        if not isinstance(update_sigma, bool):
+            raise TypeError(f"Expected 'update_sigma' to be an bool, but got {type(update_sigma).__name__}")
+
+        if not isinstance(verbose, bool):
+            raise TypeError(f"Expected 'verbose' to be an bool, but got {type(verbose).__name__}")
+
         self._params = {} 
         self.learning_rate = learning_rate
         self.base_param = base_param
@@ -68,9 +93,9 @@ class RatingSystem(ProbCalculator):
 
     def get_player_param(self, player):
         """
-        Retrieve the current rating of a player. If the player does not exist, raise an error.
+        Retrieve the current rating of a player. If the player does not exist, use the default.
 
-        :param player: The player's name or identifier.
+        :param player: The player's name.
         :return: Current rating of the player.
         """
         return self._params.get(player, self.base_param)
@@ -79,18 +104,14 @@ class RatingSystem(ProbCalculator):
 
     def _expected_prob(self, param1, param2):
         """
-        Calculate the expected prob. for Player A against Player B.
+        Calculate the expected prob. of winning a point.
 
         :param param1: Parameters of Player 1.
         :param param2: Parameters of Player 2.
         :return: Expected prob for Player 1.
         """
-
         # Calculate the combined sigma and the difference in mu values
-        sigma_tot = self._add_sigma(param1[1], param2[1])
-        mu_diff = param1[0] - param2[0]
-        z = mu_diff / sigma_tot
-
+        z = (param1[0] - param2[0]) / self._add_sigma(param1[1], param2[1])
         # Calculate the predicted probability using the logistic function
         return 1 / (1 + np.exp(-z))
 
@@ -98,15 +119,16 @@ class RatingSystem(ProbCalculator):
 
     def predict_point(self, player1, player2):
         """
-        Predict the probability of Player 1 winning against Player 2 in a set.
+        Predict the probability of Player 1 winning a point against Player 2.
 
-        :param player1: Name or identifier of Player 1.
-        :param player2: Name or identifier of Player 2.
-        :return: Probability of Player A winning.
+        :param player1: Name of Player 1.
+        :param player2: Name of Player 2.
+        :return: Probability of Player 1 winning a point.
         """
         if self.verbose:
-            for player in [player1, player2]:
-                if player not in self._params: print(f'Player "{player}" not found. initialize param: {self.base_param}')
+            for player in (player1, player2):
+                if player not in self._params: 
+                    print(f'Player "{player}" not found. Initialize param: {self.base_param}')
 
         param1 = self.get_player_param(player1)
         param2 = self.get_player_param(player2)
@@ -115,28 +137,61 @@ class RatingSystem(ProbCalculator):
 
 
     def predict_set_config(self, player1, player2):
+        """
+        Predict the probability of Player 1 winning a set against Player 2, 
+        with total points being 'n', in the set. 
+        For each possible 'n', calculate the corresponding prob.
+
+        :param player1: Name of Player 1.
+        :param player2: Name of Player 2.
+        :return: dict of probabilities of Player 1 winning the set with total 'n' points.
+        """
         p = self.predict_point(player1, player2)
         return self.predict_set_config_from_p(p)
 
     def predict_set(self, player1, player2):
+        """
+        Predict the probability of Player 1 winning a set against Player 2, 
+        
+        :param player1: Name of Player 1.
+        :param player2: Name of Player 2.
+        :return: Probability of Player 1 winning the set
+        """
         return sum(self.predict_set_config(player1, player2).values())
 
 
 
     def predict_game_config(self, player1, player2, n_win_sets=3):
+        """
+        Predict the probability of Player 1 winning the game against Player 2, 
+        with total number of sets being 'n', in the game. 
+        For each possible 'n', calculate the corresponding prob.
+
+        :param player1: Name of Player 1.
+        :param player2: Name of Player 2.
+        :param n_win_sets: Number of winning sets to win the game
+        :return: dict of probabilities of Player 1 winning the game with total 'n' sets.
+        """
         p = self.predict_set(player1, player2)
         return self.predict_game_config_from_p(p, n_win_sets)        
 
     def predict_game(self, player1, player2):
+        """
+        Predict the probability of Player 1 winning the game against Player 2, 
+        
+        :param player1: Name of Player 1.
+        :param player2: Name of Player 2.
+        :return: Probability of Player 1 winning the game
+        """
         return sum(self.predict_game_config(player1, player2).values())
 
 
 
     def _add_player(self, player, param=None):
         """
-        Add a new player with an optional custom rating.
+        Add a new player with an optional custom param.
 
-        :param player: Name or identifier of the player.
+        :param player: Name of the player.
         :param rating: Custom initial rating (defaults to base_rating if not provided).
         """
         if player not in self._params:
@@ -144,17 +199,25 @@ class RatingSystem(ProbCalculator):
 
 
 
-    def display_params(self, round_digits=2):
+    def display_params(self, round_digits=2, first_n=None):
         """
         Display the current ratings of all players, rounded to the specified number of digits.
 
         :param round_digits: Number of decimal places to round the parameters. Default is 2.
+        :param first_n: If specified, only display the top `first_n` players based on their ratings.
         """
+        # Sort players by the first element of their parameter tuple in descending order
         sorted_params = sorted(self._params.items(), key=lambda x: x[1][0], reverse=True)
+        
+        # Limit the list to the first `first_n` players if specified
+        if first_n is not None:
+            sorted_params = sorted_params[:first_n]
 
+        # Display the parameters
         for player, param in sorted_params:
             rounded_param = tuple(round(value, round_digits) for value in param)
             print(f"{player}: {rounded_param}")
+
 
 
 
@@ -167,8 +230,8 @@ class RatingSystem(ProbCalculator):
         :param result1: Result for Player 1 (points1 / points_tot).
         """
         param1 = self.get_player_param(player1)
-        param2 = self.get_player_param(player2)  # Fixed to get params for player2 instead of player1
-
+        param2 = self.get_player_param(player2)  
+        
         expected1 = self._expected_prob(param1, param2)
         mu_diff = param1[0] - param2[0]
         sigma_tot = self._add_sigma(param1[1], param2[1])
@@ -197,30 +260,43 @@ class RatingSystem(ProbCalculator):
         """
         Fit the model to a dataset of matches.
 
-        :param dataset: A list of dictionaries or a DataFrame-like object with columns:
-                        - 'player_a': Name or ID of Player A
-                        - 'player_b': Name or ID of Player B
-                        - 'result_a': Result for Player A (1 for win, 0.5 for draw, 0 for loss)
+        :param dataset: an array with shape (m, 2, s)
+                        with m = number of matches
+                             2 = 1v1
+                             s = max number of sets in the dataset
+                        for example:   
+                        array([[['Reitspies D.', 11.0, 11.0, ..., nan, nan, nan],
+                                ['Gavlas A.', 9.0, 9.0, ..., nan, nan, nan]],
+
+                            [['Kleprlik J.', 3.0, 4.0, ..., nan, nan, nan],
+                                ['Prokopcov D.', 11.0, 11.0, ..., nan, nan, nan]],
+
+                            [['Horejsi M.', 11.0, 11.0, ..., 7.0, 9.0, nan],
+                                ['Tregler T.', 4.0, 8.0, ..., 11.0, 11.0, nan]],
         """
+        # loop over matches
         for matchi in tqdm(dataset):
             matchi = matchi.T
+            # the first row are the players 
             player1, player2 = matchi[0]
-
+            
+            # loop over sets in the match
             for seti in matchi[1:]:
                 points1, points2 = seti
+                # skip nan entries
                 if np.isnan(points1) or np.isnan(points2): continue
+                # skip total points = 0
                 points_sum = points1 + points2
                 if points_sum == 0: continue
 
                 result1 = points1 / points_sum
 
-                # # Add players to the system if they are not already in
+                # Add players to the system if they are not already in
                 self._add_player(player1)
                 self._add_player(player2)
 
-                # # Update ratings based on the match result
+                # Update ratings based on the match result
                 self._update_params(player1, player2, result1)
-
 
 
 
