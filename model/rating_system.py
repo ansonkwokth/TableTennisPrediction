957c5b31445489f6920fe2a7e1d4b1f8d970219f
ansonkwokth <87415442+ansonkwokth@users.noreply.github.com>
2025-01-03 23:53:07 +0100

    Created base class for rating system

--- model/rating_system.py
+++ model/rating_system.py
@@ -3,11 +3,29 @@ from tqdm import tqdm
 from math import comb
 
 
-class ProbCalculator:
-    
+
+class RatingSystem:
+
+    def __init__(self, learning_rate=32, verbose=False):
+        
+        if not isinstance(learning_rate, (float, int)):
+            raise TypeError(f"Expected 'learning_rate' to be an float/int, but got {type(learning_rate).__name__}")
+        if learning_rate <= 0:
+            raise ValueError(f"Expected 'learning_rate' to be positive, but got {learning_rate}")
+
+        if not isinstance(verbose, bool):
+            raise TypeError(f"Expected 'verbose' to be an bool, but got {type(verbose).__name__}")
+
+        self.params = {} 
+        self.learning_rate = learning_rate
+        self.verbose = verbose
+
+
+
     @staticmethod
     def _add_sigma(sigma1, sigma2):
         return np.sqrt(sigma1**2 + sigma2**2)
+
 
 
     @staticmethod
@@ -23,6 +41,7 @@ class ProbCalculator:
         return dt
 
 
+
     @staticmethod
     def predict_game_config_from_p(p, n_win_sets=3):
         # Ensure n_win_sets is odd
@@ -36,78 +55,12 @@ class ProbCalculator:
             dt[n_win_sets + i] = prob  # Store probability for winning sets
         return dt
 
-
-
-
-
-
-
-
-
-
-# =========================================================================================================================
-# =========================================================================================================================
-# =========================================================================================================================
-class Elo(ProbCalculator):
-    def __init__(self, learning_rate=32, base_rate=1000, verbose=False):
-        
-        if not isinstance(learning_rate, (float, int)):
-            raise TypeError(f"Expected 'learning_rate' to be an float/int, but got {type(learning_rate).__name__}")
-        if learning_rate <= 0:
-            raise ValueError(f"Expected 'learning_rate' to be positive, but got {learning_rate}")
-
-        if not isinstance(base_rate, (float, int)):
-            raise TypeError(f"Expected 'base_rate' to be an float/int, but got {type(base_rate).__name__}")
-        
-        if not isinstance(verbose, bool):
-            raise TypeError(f"Expected 'verbose' to be an bool, but got {type(verbose).__name__}")
-
-        self.params = {} 
-        self.learning_rate = learning_rate
-        self.base_rate = base_rate
-        self.verbose = verbose
-
-
-
-    def __repr__(self):
-        return f"Elo(learning_rate={self.learning_rate}, base_rate={self.base_rate}, verbose={self.verbose})"
-        
-
-
-
-
+  
 
     def get_player_param(self, player):
-        """
-        Retrieve the current rating of a player. If the player does not exist, use the default.
-
-        :param player: The player's name.
-        :return: Current rating of the player.
-        """
-        return self.params.get(player, self.base_rate)
+        return self._get_player_param(player)
 
 
-
-    def _expected_prob(self, param1, param2):
-        """
-        Calculate the expected prob. of winning a point.
-
-        :param param1: Parameters of Player 1.
-        :param param2: Parameters of Player 2.
-        :return: Expected prob for Player 1.
-        """
-        # Calculate the combined sigma and the difference in mu values
-        z = (param1 - param2) / 400
-        # Calculate the predicted probability using the logistic function
-        return 1 / (1 + 10**(-z))
-
-
-    
-    def _prediction_verbose(self, player1, player2):
-        if self.verbose:
-            for player in (player1, player2):
-                if player not in self.params: 
-                    print(f'Player "{player}" not found. Initialize param: {self.base_rate}')
 
     def predict_point(self, player1, player2):
         """
@@ -176,64 +129,7 @@ class Elo(ProbCalculator):
         :return: Probability of Player 1 winning the game
         """
         return sum(self.predict_game_config(player1, player2).values())
-    
 
-
-    def _add_player(self, player, rate=None):
-        """
-        Add a new player with an optional custom param.
-
-        :param player: Name of the player.
-        :param rating: Custom initial rating (defaults to base_rating if not provided).
-        """
-        if player not in self.params:
-            self.params[player] = rate if rate is not None else self.base_rate
-
-
-
-    def display_params(self, first_n=None):
-        """
-        Display the current ratings of all players, rounded to the specified number of digits.
-
-        :param first_n: If specified, only display the top `first_n` players based on their ratings.
-        """
-        # Sort players by the first element of their parameter tuple in descending order
-        sorted_params = sorted(self.params.items(), key=lambda x: x[1], reverse=True)
-        
-        # Limit the list to the first `first_n` players if specified
-        if first_n is not None:
-            sorted_params = sorted_params[:first_n]
-
-        # Display the parameters
-        for player, rate in sorted_params:
-            print(f"{player}: {rate}")
-
-
-
-    def _update_params(self, player1, player2, result1):
-        """
-        Update ratings for two players after a match.
-
-        :param player1: Name or identifier of Player 1.
-        :param player2: Name or identifier of Player 2.
-        :param result1: Result for Player 1 (points1 / points_tot).
-        """
-        param1 = self.get_player_param(player1)
-        param2 = self.get_player_param(player2)  
-        
-        expected1 = self._expected_prob(param1, param2)
-        mu_diff = param1 - param2
-
-        result1 = 1 if result1 > 0.5 else 0
-
-        # Calculate updates
-        mu1_update = (result1 - expected1)
-        mu2_update = ((1-result1) - expected1)
-        
-        # Update ratings and assign new tuples
-        self.params[player1] = param1 + self.learning_rate * mu1_update
-        self.params[player2] = param2 + self.learning_rate * mu2_update
-        
 
 
     def fit(self, dataset):
@@ -256,7 +152,7 @@ class Elo(ProbCalculator):
                                 ['Tregler T.', 4.0, 8.0, ..., 11.0, 11.0, nan]],
         """
         # loop over matches
-        for matchi in tqdm(dataset):
+        for matchi in tqdm(dataset, desc="Training model"):
             matchi = matchi.T
             # the first row are the players 
             player1, player2 = matchi[0]
@@ -278,156 +174,4 @@ class Elo(ProbCalculator):
 
                 # Update ratings based on the match result
                 self._update_params(player1, player2, result1)
-
-
-
-        
-        
-
-
-
-
-
-
-# =========================================================================================================================
-# =========================================================================================================================
-# =========================================================================================================================
-class EloLike(Elo):
-    def __init__(self, learning_rate=0.1, base_param=(0, 1), update_sigma=True, verbose=False):
-        """
-        Initialize the rating system.
-
-        :param learning_rate: Learning rate.
-        :param params: Parameters for the players.
-        """
-        super().__init__(learning_rate=learning_rate, base_rate=base_param[0], verbose=verbose)
-
-        if not isinstance(base_param, tuple):
-            raise TypeError(f"Expected 'base_param' to be a tuple, but got {type(base_param).__name__}")
-        if len(base_param) != 2:
-            raise ValueError(f"Expected 'value' to be a 2-entry tuple, but got {len(base_param)} entries")
-        if not all(isinstance(entry, (int, float)) for entry in base_param):
-            raise TypeError("Both entries in 'base_param' must be float/int")
-        if base_param[1] <= 0:
-            raise ValueError(f"The second entry in 'base_param' must be greater than 0, but got {base_param[1]}")
-        
-        if not isinstance(update_sigma, bool):
-            raise TypeError(f"Expected 'update_sigma' to be an bool, but got {type(update_sigma).__name__}")
-
-        self.params = {}
-        self.base_param = base_param        
-        self.update_sigma = update_sigma
-
-
-        
-    def __repr__(self):
-        return f"EloLike(learning_rate={self.learning_rate}, base_param={self.base_param}, update_sigma={self.update_sigma}, verbose={self.verbose})"
-       
-
-
-
-
-
-    def get_player_param(self, player):
-        """
-        Retrieve the current rating of a player. If the player does not exist, use the default.
-
-        :param player: The player's name.
-        :return: Current rating of the player.
-        """
-        return self.params.get(player, self.base_param)
-
-
-
-
-    def _expected_prob(self, param1, param2):
-        """
-        Calculate the expected prob. of winning a point.
-
-        :param param1: Parameters of Player 1.
-        :param param2: Parameters of Player 2.
-        :return: Expected prob for Player 1.
-        """
-        # Calculate the combined sigma and the difference in mu values
-        z = (param1[0] - param2[0]) / self._add_sigma(param1[1], param2[1])
-        # Calculate the predicted probability using the logistic function
-        return 1 / (1 + np.exp(-z))
-
-
-
-    def _prediction_verbose(self, player1, player2):
-        if self.verbose:
-            for player in (player1, player2):
-                if player not in self.params: 
-                    print(f'Player "{player}" not found. Initialize param: {self.base_param}')
-
-
-
-    def _add_player(self, player, param=None):
-        """
-        Add a new player with an optional custom param.
-
-        :param player: Name of the player.
-        :param rating: Custom initial rating (defaults to base_rating if not provided).
-        """
-        if player not in self.params:
-            self.params[player] = param if param is not None else self.base_param
-
-
-
-    def display_params(self, round_digits=2, first_n=None):
-        """
-        Display the current ratings of all players, rounded to the specified number of digits.
-
-        :param round_digits: Number of decimal places to round the parameters. Default is 2.
-        :param first_n: If specified, only display the top `first_n` players based on their ratings.
-        """
-        # Sort players by the first element of their parameter tuple in descending order
-        sorted_params = sorted(self.params.items(), key=lambda x: x[1][0], reverse=True)
-        
-        # Limit the list to the first `first_n` players if specified
-        if first_n is not None:
-            sorted_params = sorted_params[:first_n]
-
-        # Display the parameters
-        for player, param in sorted_params:
-            rounded_param = tuple(round(value, round_digits) for value in param)
-            print(f"{player}: {rounded_param}")
-
-
-
-    def _update_params(self, player1, player2, result1):
-        """
-        Update ratings for two players after a match.
-
-        :param player1: Name or identifier of Player 1.
-        :param player2: Name or identifier of Player 2.
-        :param result1: Result for Player 1 (points1 / points_tot).
-        """
-        param1 = self.get_player_param(player1)
-        param2 = self.get_player_param(player2)
-        
-        expected1 = self._expected_prob(param1, param2)
-        mu_diff = param1[0] - param2[0]
-        sigma_tot = self._add_sigma(param1[1], param2[1])
-
-        # Calculate updates
-        mu1_update = (expected1 - result1) / sigma_tot
-        mu2_update = - (expected1 - result1) / sigma_tot
-        sigma1_update = sigma2_update = 0
-        if self.update_sigma:
-            sigma1_update = (-mu_diff * param1[1]) / sigma_tot**3 * (expected1 - result1)
-            sigma2_update = (-mu_diff * param2[1]) / sigma_tot**3 * (expected1 - result1)
-
-        # Update ratings and assign new tuples
-        self.params[player1] = (
-            param1[0] - self.learning_rate * mu1_update,
-            param1[1] - self.learning_rate * sigma1_update
-        )
-        self.params[player2] = (
-            param2[0] - self.learning_rate * mu2_update,
-            param2[1] - self.learning_rate * sigma2_update
-        )
-
-
 
